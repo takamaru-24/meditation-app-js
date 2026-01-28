d2c2090bdc4b1eed8a468e6a11d5266452160d63
井手貴政 <taltuka26maruko9@gmail.com>
2026-01-28 11:05:05 +0900

    開始・終了ボタンの実装

--- app.js
+++ app.js
@@ -1,25 +1,83 @@
 const container = document.querySelector(".container");
-const text = document.getElementById("text")
-
+const text = document.getElementById("text");
+const toggleBtn = document.getElementById("toggleBtn");
+const pointerContainer = document.querySelector(".pointer-container");
 
 let totalTime = 7500;
 let breathTime = (totalTime / 5) * 2;
 let holdTime = totalTime / 5;
 
-meditateAnimation();
+let isRunning = false;
+let intervalId = null;
+let timeoutIds = [];
 
-    function meditateAnimation() {
-        text.innerHTML = "吸って！";
-        container.className = "container grow";
-        setTimeout(() => {
-            text.innerHTML = "止めて！";
+toggleBtn.addEventListener("click", toggleMeditation);
 
-            setTimeout(() => {
-                text.innerHTML = "吐いて！"
-                container.className = "container shrink";
-            }, holdTime);
-        }, breathTime);
+function toggleMeditation() {
+    if (isRunning) {
+        stopMeditation();
+    } else {
+        startMeditation();
     }
+}
 
-    setInterval(meditateAnimation, totalTime);
+function startMeditation() {
+    isRunning = true;
+    toggleBtn.textContent = "瞑想終了";
+    pointerContainer.classList.add("rotating");
+    
+    meditateAnimation();
+    intervalId = setInterval(meditateAnimation, totalTime);
+}
 
+function stopMeditation() {
+    isRunning = false;
+    toggleBtn.textContent = "瞑想開始";
+    
+    // インターバルをクリア
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+    
+    // すべてのタイムアウトをクリア
+    timeoutIds.forEach(id => clearTimeout(id));
+    timeoutIds = [];
+    
+    // 初期状態に戻す
+    resetToInitialState();
+}
+
+function resetToInitialState() {
+    text.innerHTML = "準備完了";
+    container.className = "container";
+    pointerContainer.classList.remove("rotating");
+    pointerContainer.style.animation = "none";
+    
+    // アニメーションをリセットするために少し待ってから再適用
+    setTimeout(() => {
+        pointerContainer.style.animation = "";
+    }, 10);
+}
+
+function meditateAnimation() {
+    // 前のタイムアウトをクリア
+    timeoutIds.forEach(id => clearTimeout(id));
+    timeoutIds = [];
+    
+    text.innerHTML = "吸って!";
+    container.className = "container grow";
+    
+    const timeout1 = setTimeout(() => {
+        text.innerHTML = "止めて!";
+
+        const timeout2 = setTimeout(() => {
+            text.innerHTML = "吐いて!"
+            container.className = "container shrink";
+        }, holdTime);
+        
+        timeoutIds.push(timeout2);
+    }, breathTime);
+    
+    timeoutIds.push(timeout1);
+}
