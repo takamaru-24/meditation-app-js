2fdc4ddff59a7ca0f48054cba19c1ad661c2b9ae
井手貴政 <taltuka26maruko9@gmail.com>
2026-01-28 10:51:05 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+const text = document.getElementById("text")
+
+
+let totalTime = 7500;
+let breathTime = (totalTime / 5) * 2;
+let holdTime = totalTime / 5;
+
+meditateAnimation();
+
+    function meditateAnimation() {
+        text.innerHTML = "吸って！";
+        container.className = "container grow";
+        setTimeout(() => {
+            text.innerHTML = "止めて！";
+
+            setTimeout(() => {
+                text.innerHTML = "吐いて！"
+                container.className = "container shrink";
+            }, holdTime);
+        }, breathTime);
+    }
+
+    setInterval(meditateAnimation, totalTime);
+
